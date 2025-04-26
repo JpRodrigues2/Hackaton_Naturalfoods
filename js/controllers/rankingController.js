11c090da2733397636378d5ba30a466a915d0d50
LucasAlves71 <lucasalves2005@gmail.com>
2025-04-26 15:12:57 -0300

    feat.. adds ranking

--- js/controllers/rankingController.js
+++ js/controllers/rankingController.js
@@ -1,30 +1,108 @@
-angular.module('naturalFoodsApp', [])
-.controller('DashboardController', function($scope, $http) {
+var app = angular.module('app', []);
+
+app.controller('RankingController', function($scope, $http) {
+  // Set current page for nav highlighting
+  $scope.currentPage = window.location.pathname.split('/').pop();
+  
+  // Navigation function
+  $scope.navigateTo = function(page) {
+    window.location.href = page;
+  };
+  
+  // Logout function
+  $scope.logout = function() {
+    localStorage.removeItem("userId");
+    localStorage.removeItem("userToken");
+    window.location.href = "login.html";
+  };
+  
+  // Loading state
   $scope.loading = true;
   $scope.error = false;
-
-  var userId = localStorage.getItem('userId');
-
+  
   var req = {
-    method: 'GET',
-    url: 'https://service2.funifier.com/v3/player/' + userId + '/status',
+    method: 'POST',
+    url: 'https://service2.funifier.com/v3/database/team/aggregate',
     headers: {
-      "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsImNhbGciOiJHWklQIn0.H4sIAAAAAAAAAD2LwQoCIRgGXyU8e2j9TaN7EHTsAeJb_RcESVG3WqJ3zwr2NgwzL4EczryIgzD77ejZQZGyk9UTgSwYLKSoLmXuSWH4K2KUm0cJjf_oOfLKaBhRvw-cS_Ot9et4WU7uXrqbK5dVpC4CejBYvTOkSBsp-Jl_wiiygzbvD3ZQkXOgAAAA.FR3y3sWGfdtXObKzZPPuLUAkIhbYeNuWG5ukclGbGPIP-hZXpXwbut0U0xodQqIslfqN0As2-oLukYnzsbkZZA",
+      "Authorization": "Bearer eyJhbGciOiJIUzUxMiIsImNhbGciOiJHWklQIn0.H4sIAAAAAAAAAD2LSwrCMBQAryJvnUWTNB_cC0KXHkBek1cIBBOSVC3i3Y0K3Q3DzAswh4k2OIK2w-zJoZDCLGZcJEqDhAQMqkuZelII_RVjZIdHCY3-6CnSzthwxvp90Lm03lq_Tpft7O6lu7VS2UXqImAPuBmV5oPSlgE9809owZXU9v0Bw-zfGKAAAAA.dtVE252a_UspezdZW0FZjWmKy3kLFNMAVfdJGN-NFf8tn78XQfJUkzYTU0h145lHpPwJMQ9qMBg4hNL3o3iy0Q",
       "Content-Type": "application/json"
-    }
+    },
+    data: [
+      {
+        "$lookup": {
+          "from": "achievement",
+          "localField": "_id",
+          "foreignField": "player",
+          "as": "achievements"
+        }
+      },
+      {
+        "$addFields": {
+          "xp_total": {
+            "$sum": {
+              "$map": {
+                "input": {
+                  "$filter": {
+                    "input": "$achievements",
+                    "as": "ach",
+                    "cond": { "$eq": ["$$ach.item", "xp"] }
+                  }
+                },
+                "as": "xp",
+                "in": "$$xp.total"
+              }
+            }
+          }
+        }
+      },
+      {
+        "$project": {
+          "_id": 1,
+          "name": 1,
+          "image": 1,
+          "created": 1,
+          "updated": 1,
+          "xp_total": 1
+        }
+      },
+      {
+        "$setWindowFields": {
+          "sortBy": { "xp_total": -1 },
+          "output": {
+            "rank": {
+              "$documentNumber": {}
+            }
+          }
+        }
+      },
+      {
+        "$project": {
+          "_id": 1,
+          "name": 1,
+          "image": "$image.small.url",
+          "total": "$xp_total",
+          "position": "$rank"
+        }
+      }
+    ]
   };
 
-  $http(req).then(function(response) {
-    $scope.player = response.data;
-    $scope.loading = false;
-  }, function(error) {
-    console.error(error);
-    $scope.error = true;
-    $scope.loading = false;
-  });
-
-  $scope.logout = function() {
-    localStorage.removeItem('userId');
-    window.location.href = 'login.html';
-  }
+  $http(req).then(
+    function (response) {
+      $scope.teams = response.data;
+      console.log($scope.teams);
+      $scope.loading = false;
+      
+      // Prepare data for podium display
+      if($scope.teams && $scope.teams.length > 0) {
+        $scope.topThree = $scope.teams.slice(0, 3);
+        $scope.otherTeams = $scope.teams.slice(3);
+      }
+    },
+    function (error) {
+      console.error('Erro ao buscar o ranking:', error);
+      $scope.loading = false;
+      $scope.error = true;
+    }
+  );
 });
